62c6b4c221d4110f79161299a49178f41e88f772
Kenzo Miranda Sakiyama <kenzosakiyama@gmail.com>
2020-07-21 13:36:35 -0400

    Finalizando a análise do texto em geral

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,16 @@
 import pandas as pd
+import spacy
+
 from textacy.preprocessing import normalize_hyphenated_words, normalize_unicode, normalize_whitespace
 from textacy.preprocessing import remove_accents, remove_punctuation
+from textacy.spacier.doc_extensions import to_bag_of_words, to_bag_of_terms
+
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+
+from typing import List
+
+import matplotlib.pyplot as plt
 
 text_processing_pipeline = [
     normalize_hyphenated_words, # remoção de hífens dentro de palavras
@@ -15,5 +25,39 @@ def apply_text_processing_pipeline(df: pd.Series) -> pd.Series:
     
     for func in text_processing_pipeline:
         df = df.apply(func)
-
+    # Removendo string comum
+    df = df.apply(lambda text: text.replace("[This review was collected as part of a promotion.]", ""))
     return df
+
+def get_word_frequency_df(raw_text: spacy.language.Doc) -> pd.DataFrame:
+
+    word_dict = to_bag_of_words(raw_text, filter_stops=True, filter_punct=True, as_strings=True)
+
+    word_freq_df = pd.DataFrame(index=word_dict.keys(), data=word_dict.values(), columns=["freq"])
+    word_freq_df.drop("-PRON-", axis=0, inplace=True) # special token
+    word_freq_df.sort_values(by="freq", ascending=False, inplace=True)
+
+    return word_freq_df
+
+def get_ngram_df(raw_text: spacy.language.Doc, n: tuple = (2,3,4)) -> pd.DataFrame:
+
+    ngrams_dict = to_bag_of_terms(raw_text, ngrams=n, filter_stops=True, filter_punct=True, as_strings=True)
+    
+    ngram_df = pd.DataFrame(index=ngrams_dict.keys(), data=ngrams_dict.values(), columns=["freq"])
+    ngram_df.drop(index="review be collect", inplace=True) # frase recorrente
+    ngram_df.sort_values(by="freq", ascending=False, inplace=True)
+
+    return ngram_df
+
+def build_wordcloud(text: str, additional_stop_words: List[str], output_file: str = "wordcloud.png", save_file: bool = False) -> None:
+
+    stop_words = stopwords.words("english")
+    stop_words.extend([word.lower() for word in additional_stop_words])
+
+    wc = WordCloud(colormap='binary', max_words=200, stopwords=stop_words, background_color='white', width=1600, height=800)
+    cloud = wc.generate(text.lower())
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.axis("off")
+    # plt.close()
+
+    if save_file: wc.to_file(output_file)
